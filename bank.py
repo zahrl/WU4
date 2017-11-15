b6efeb656416e946080897396dba2d3371c27794
Dr. Paul R. Zahrl <prz@paulrzahrl.com>
2017-11-15 19:46:26 +0100

    chg: Add owner field

--- bank.py
+++ bank.py
@@ -5,20 +5,21 @@ class BankAccount:
 		self.account_number = BankAccount.current_number_of_bank_accounts
 		BankAccount.current_number_of_bank_accounts += 1;
 		self.balance = 0
-		print(self.return_account_number() + "New account opened!" + self.return_balance())
+		self.owner = input("Enter name of account holder: ")
+		print(self.return_account_number_and_owner() + "New account opened!" + self.return_balance())
 
 	def withdraw(self, amount):
 		self.balance -= amount
-		print(self.return_account_number() + str(amount) + " withdrawn!" + self.return_balance())
+		print(self.return_account_number_and_owner() + str(amount) + " withdrawn!" + self.return_balance())
 		return self.balance
 
 	def deposit(self, amount):
 		self.balance += amount
-		print(self.return_account_number() + str(amount) + " deposited!" + self.return_balance())
+		print(self.return_account_number_and_owner() + str(amount) + " deposited!" + self.return_balance())
 		return self.balance
 
-	def return_account_number(self):
-		return str("Acc.no. " + str(self.account_number) + ": ")
+	def return_account_number_and_owner(self):
+		return str("Acc.no. " + str(self.account_number) + " (Acc.holder: " + self.owner + "): ")
 
 	def return_balance(self):
 		return str(" Balance: " + str(self.balance))
