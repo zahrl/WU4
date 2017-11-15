8bcee5a540a07dae51eeea7874950cc9156c07f8
Paul Rudolf Zahrl <h0607536@green9.wu-wien.ac.at>
2017-11-15 15:35:43 +0100

    add: Utilize list

---
+++ list.py
@@ -0,0 +1,15 @@
+shopping_list = ['Milk', 'Apples', 'Eggs', 'Toilet rolls', 'Bananas', 'Bread']
+
+print(shopping_list[1])
+print(shopping_list[-2])
+print(shopping_list[0:2])
+print(shopping_list[0:-2])
+print('Milk' in shopping_list)
+
+for x in shopping_list:
+	print("Get " + x)
+
+for x in range(3):
+	print("The 'count' is: ", x)
+
+print('single quotation marks do not need to escape "double quotation" mark')
