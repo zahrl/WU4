4eae87342d88126b98904ab36bcd3bbecf2e3671
Dr. Paul R. Zahrl <prz@paulrzahrl.com>
2017-11-16 19:50:58 +0100

    chg: Append, insert, remove elements to list; FIFO/LIFO

--- list.py
+++ list.py
@@ -1,10 +1,19 @@
-shopping_list = ['Milk', 'Apples', 'Eggs', 'Toilet rolls', 'Bananas', 'Bread']
+shopping_list = ['Milk', 'Apples', 'Eggs', 'Honey', 'Bananas', 'Bread']
 
 print(shopping_list[1])
 print(shopping_list[-2])
 print(shopping_list[0:2])
 print(shopping_list[0:-2])
 print('Milk' in shopping_list)
+
+print("FIFO: " + shopping_list.pop(0))
+
+shopping_list.append("Dark Chocolate")
+shopping_list.insert(0, "Blueberries")
+shopping_list.insert(0, "Chocolate")
+shopping_list.remove("Chocolate")
+
+print("LIFO: " + shopping_list.pop())
 
 for x in shopping_list:
 	print("Get " + x)
