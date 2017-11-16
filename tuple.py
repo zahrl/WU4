dc9b400a165f99f6b9abf2e982d5c3626b4d0004
Dr. Paul R. Zahrl <prz@paulrzahrl.com>
2017-11-16 21:07:42 +0100

    chg: Split string as tuple

--- tuple.py
+++ tuple.py
@@ -1,6 +1,14 @@
 tuple = ("Beverly Hills", 90210, "California", "USA")
 (city, zip, state, country) = tuple
+print(tuple)
 print(city)
 print(zip)
 print(state)
 print(country)
+
+prz_string = "Paul R. Zahrl"
+(x, y, z) = prz_string.split()
+print((x, y, z))
+print(x)
+print(y)
+print(z)
