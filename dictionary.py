6e1f45663d43c0d7857454974cc77c6b325c0f06
Dr. Paul R. Zahrl <prz@paulrzahrl.com>
2017-11-16 21:58:40 +0100

    add: Utilize dictionary

---
+++ dictionary.py
@@ -0,0 +1,15 @@
+french = dict()
+french['hello'] = 'bonjour'
+french['yes'] = 'oui'
+french['one'] = 'un(e)'
+french['two'] = 'deux'
+french['three'] = 'trois'
+french['red'] = 'rouge'
+french['green'] = 'vert'
+french['blue'] = 'bleu'
+french['love'] = 'amour'
+french['leg'] = 'jambe'
+french['hair'] = 'cheveux'
+french['hand'] = 'main'
+
+print(french['red'])
